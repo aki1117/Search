7205b7f8ab7a1b52ee603afb81a7a09b25e9259d
Abdul Eriawan <abdul.eriawan.ft18@mail.umy.ac.id>
2022-10-28 17:07:33 +0700

    Main

--- Program.cs
+++ Program.cs
@@ -103,7 +103,42 @@ namespace Search
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program myList = new Program();
+            int pilihanmenu;
+            char ch;
+
+            Console.WriteLine("Menu Option");
+            Console.WriteLine("===============");
+            Console.WriteLine("1. Linear Search");
+            Console.WriteLine("2. Binary Search");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine("Enter your choice (1,2,3) :");
+            pilihanmenu = Convert.ToInt32(Console.Read());
+            switch (pilihanmenu)
+            {
+                case 1:
+                    Console.WriteLine("");
+                    Console.WriteLine("=================");
+                    Console.WriteLine("Linear Search");
+                    Console.WriteLine("=================");
+                    myList.input();
+                    myList.LinearSearch();
+                    break;
+                case 2:
+                    Console.WriteLine("");
+                    Console.WriteLine("=================");
+                    Console.WriteLine("Binary Search");
+                    Console.WriteLine("=================");
+                    myList.input();
+                    myList.BinarySearch();
+                    break;
+                case 3:
+                    Console.WriteLine("Exit");                   
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
         }
     }
 }
