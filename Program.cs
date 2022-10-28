2657d0033a1a890548e413eb394d52b81902d8c4
Abdul Eriawan <abdul.eriawan.ft18@mail.umy.ac.id>
2022-10-28 16:32:42 +0700

    binary search

--- Program.cs
+++ Program.cs
@@ -20,8 +20,57 @@ namespace Search
                 if ((n > 0) && (n <= 20))
                     break;
                 else
-                    Console.WriteLine("\n Array should have minimum 1 and maximum 20 elements. \n")
+                    Console.WriteLine("\n Array should have minimum 1 and maximum 20 elements. \n");
             }
+            //Accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("=======================");
+            Console.WriteLine(" Enter array elements. ");
+            Console.WriteLine("=======================");
+            for(i = 0; i<n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
+
+        public void BinarySearch()
+        {
+            char ch;
+            do
+            {
+                //accept the number to be searched
+                Console.Write("\nEnter element you want to search: ");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                //apply binary sarch
+                int lowerbound = 0;
+                int upperbound = n - 1;
+
+                //obtain the index of the element in the array
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                //loop to search for the elements in the array
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "found at position" + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array\n");
+                Console.WriteLine("\nNumber of comparison : " + ctr);
+
+                Console.Write("\nContinue Search? (y/n):");
+                ch = char.Parse(Console.ReadLine());
+            } while ((ch == 'y') || (ch == 'Y'));
         }
         static void Main(string[] args)
         {
